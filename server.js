a59f6e285a548af6b35264410cbff471ba7cfea9
drew <drewlearns@hey.com>
2022-11-27 10:44:51 -0500

    3.3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = new express();
 const port = 8080;
-
+app.use(express.static('public')) 
 app.listen(port, ()=>{
     console.log(`Seving photo app on http://localhost:${port}`);
 })
