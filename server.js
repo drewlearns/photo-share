672de2da0cba039e7df43d6f9123c27bfaed3293
drew <drewlearns@hey.com>
2022-11-28 04:15:19 -0500

    chp5

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const { response } = require('express');
 const express = require('express');
 const app = new express();
-const port = 8080;
+const db = require('./models');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 app.use(express.static('public')) 
 app.set("view engine", "ejs")
+
+//db
+const sqlPort = 3307; // 3306 or 3307
+db.sequelize
+    .sync({})
+    .then(()=>{
+        app.listen(sqlPort, ()=>{
+            console.log(
+                `Mariadb Connection Successful - http://localhost:${sqlPort}.`
+            )
+        })
+    })
+    .catch((error) => {
+        console.error("Unable to connect to the database", error)
+    })
+
+//server
+const port = 8080;
 app.listen(port, ()=>{
     console.log(`Seving photo app on http://localhost:${port}`);
 })
 
+
+//routes
 app.get("/", (request, response) =>{
     response.render("index");
 })
