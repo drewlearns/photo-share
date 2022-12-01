b47e59002d7dc72ab256c393dd5228e4f626f6ad
drewlearns <drewlearns@hey.com>
2022-12-01 15:58:46 -0500

    fix

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,56 +1,60 @@
-const express = require('express');
+const { request, response } = require("express");
+const express = require("express");
 const UsersRouter = express.Router();
-const db = require('../models');
-const bodyParser = require('body-parser');
-UsersRouter.use(bodyParser.urlencoded())
-UsersRouter.use(bodyParser.json())
-const bcrypt = require('bcryptjs');
+const db = require("../models");
+const bodyParser = require("body-parser");
+UsersRouter.use(bodyParser.urlencoded());
+UsersRouter.use(bodyParser.json());
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 UsersRouter.route("/login").post(async (request, response) => {
     // username and password are required
     const username = request.body.username;
     const password = request.body.password;
-    const encryptedPassword = await bcrypt.hash(password, saltRounds);
     db.user
       .findOne({ where: { username: username } })
       .then(async (user) => {
         if (user) {
-          await bcrypt.compare(password, user.password, (error, same) => {
+          bcrypt.compare(password, user.password, (error, same) => {
             if (same) {
-              // console.log(user.id)
-              request.session.userId = user.id; // THIS LINE HERE!!!!!!!!!
-              console.log(request.session)
-              console.log("logged in")
+              request.session.userId = user.id; 
               response.redirect("/");
             } else {
-              response.status(401)
-              console.log("401 error")
-              response.redirect("/badlogin");
+              alert("Wrong Password")
+              response.redirect("/login");
             }
           });
-        }else{
-          response.send("No such user")
+        }
+        else {
+          response.status(401)
+          console.log("401 error")
+          response.redirect("/badlogin")
         }
       })
       .catch((error) => {
-        console.log(error);
+        console.log("this fired", error);
         response.send(error);
       });
   });
-UsersRouter.route('/signup')
-    .post(async (request, response) => {
-        const email = request.body.email
-        const password = request.body.password
-        const encryptedPassword = await bcrypt.hash(password, saltRounds);
-        const username = request.body.username
-        db.user.create({ email: email[0], username: username, password: encryptedPassword }).then(user => {
-            // response.send(user)
-            response.redirect('/login')
-        }).catch((error) => {
-            response.send("You do not have an account. Try signing up!")
-        }
-        )
-    })
+
+UsersRouter.route("/signUp").post(async (request, response) => {
+  const password = request.body.password;
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  // email, password, username
+  const email = request.body.email;
+  const username = request.body.username;
+//   console.log(encryptedPassword)
+  db.user
+    .create({ email: email[0], password: encryptedPassword, username: username })
+      //response.send(user) // changed in chapter 7.2
+      .then((user) => {
+        //response.send(user) // changed in chapter 7.2
+        response.redirect("/login");
+      })
+      .catch((err) => {
+        err;
+      });
+  });
 
 module.exports = UsersRouter;
--- server.js
+++ server.js
@@ -1,50 +1,47 @@
-const { response, application } = require('express');
-const express = require('express');
+const express = require("express");
 const app = new express();
-const db = require('./models');
-const bodyParser = require('body-parser');
-const logger = require("morgan")
-const expressSession = require('express-session');
-app.use(expressSession({secret: "Drew Loves Kinsta"}))
+const port = 8080;
+const db = require("./models");
+const expressSession = require("express-session");
+const logger = require("morgan");
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(expressSession({
+  secret: 'Drew Loves Kinsta'
+}))
+app.listen(port, () => {
+  console.log(`Serving photo app on http://localhost:${port}`);
+});
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const sqlPort = 3306;
+// set the view engine to ejs
+app.set("view engine", "ejs");
 global.loggiedIn = null;
 app.use("*", (request, response, next) => {
   loggedIn = request.session.userId;
   next();
 });
 
-app.use(bodyParser.json())
-app.use(logger("dev"))
-app.use(express.static('public')) 
-app.set("view engine", "ejs")
-
-const PhotosRouter = require('./routes/PhotosRouter')
-const UsersRouter = require('./routes/UsersRouter')
-const CommentsRouter = require('./routes/CommentsRouter')
-const PageRouter = require('./routes/PageRouter')
-app.use('/images', PhotosRouter)
-app.use('/comments', CommentsRouter)
-app.use('/users', UsersRouter)
-app.use('/', PageRouter)
-
 //db
-const sqlPort = 3306; // 3306 or 3307
 db.sequelize
-    .sync()
-    .then(()=>{
-        app.listen(sqlPort, ()=>{
-            console.log(
-                `Mariadb Connection Successful - http://localhost:${sqlPort}.`
-            )
-        })
-    })
-    .catch((error) => {
-        console.error("Unable to connect to the database", error)
-    })
+  .sync()
+  .then(() => {
+    app.listen(sqlPort, () => {
+      console.log(
+        `MariaDB Connection has been established successfully to http://localhost:${sqlPort}.`
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
 
-//server
-const port = 8080;
-app.listen(port, ()=>{
-    console.log(`Serving photo app on http://localhost:${port}`);
-})
-
-
+  const PhotosRouter = require('./routes/PhotosRouter')
+  const CommentsRouter = require('./routes/CommentsRouter')
+  const UsersRouter = require('./routes/UsersRouter')
+  const PageRouter = require('./routes/PageRouter')
+  app.use("/images", PhotosRouter);
+  app.use("/comments", CommentsRouter);
+  app.use("/users", UsersRouter);
+  app.use('/', PageRouter)
